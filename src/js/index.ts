fab953148149ebe9719e69007f4741a412f63bb3
Christoffer A Træen <christoffer.andersen@live.com>
2020-06-27 21:58:22 +0200

    Working demo with randomness

--- src/js/index.ts
+++ src/js/index.ts
@@ -7,13 +7,61 @@ async function start() {
   const width = 900
   const height = 900
   const radius = Math.min(width - 200, height) / 2
-  const color = d3.scaleOrdinal(d3.schemeAccent)
 
-  const graph = d3
+  function fuzy(data: any, nodata: number = 0) {
+    let noData = Math.random()
+    let noTarget = Math.random()
+    if (data.children) {
+      if ((noData < 0.1 || noTarget < 0.1) && nodata == 0) {
+        if (noData < noTarget) {
+          nodata = -1
+          data.noData = true
+        } else {
+          nodata = 1
+          data.noTarget = true
+        }
+      }
+      for (const iterator of data.children) {
+        fuzy(iterator, nodata)
+      }
+    } else if (data.value) {
+      if ((noData < 0.1 || noTarget < 0.1) && nodata == 0) {
+        if (noData < noTarget) {
+          data.noData = true
+        } else {
+          data.noTarget = true
+        }
+        data.value = 20
+      } else if (nodata != 0) {
+        data.value = 20
+        if (nodata < 0) data.noData = true
+        if (nodata > 0) data.noTarget = true
+      } else {
+        let rnd = Number.parseInt((Math.random() * 100).toFixed(0))
+        data.value = rnd
+      }
+    }
+  }
+  fuzy(nodeData, 0)
+
+  var e = new Array<Range>()
+  var color = d3
+    .scaleThreshold<number, string>()
+    .domain([32, 66, 95])
+    .range(['#d17d52', '#d8ae56', '#72b282', '#459a71'])
+
+  var colorNoValue = d3
+    .scaleThreshold<number, string>()
+    .domain([0])
+    .range(['#89817b', '#609fba'])
+
+  const svg = d3
     .select('svg') // Selects an element; SVG element
     .attr('width', width)
     .attr('height', height)
     .style('font', '10px sans-serif')
+
+  const g = svg
     .append('g')
     .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')') // <-- 4
 
@@ -22,6 +70,29 @@ async function start() {
   })
   const partition = d3.partition().size([2 * Math.PI, radius])
   partition(root)
+
+  console.log(root)
+
+  function nullifyNoDataFields(data: any) {
+    if (data.children) {
+      if (data.data.noData || data.data.noTarget) {
+        data.value = 0
+      }
+      for (const iterator of data.children) {
+        let val = nullifyNoDataFields(iterator)
+        if (data.value > 0) {
+          data.value -= val
+        }
+      }
+    } else if (data.value && (data.data.noData || data.data.noTarget)) {
+      let oldval = data.value
+      data.value = 0
+      return oldval
+    }
+    return 0
+  }
+
+  nullifyNoDataFields(root)
 
   var arc = d3
     .arc()
@@ -40,8 +111,7 @@ async function start() {
       return d.y1
     })
 
-  graph
-    .selectAll('g')
+  g.selectAll('g')
     .data(root.descendants())
     .enter()
     .append('g')
@@ -52,10 +122,20 @@ async function start() {
     })
     .attr('d', arc as any)
     .style('stroke', '#fff')
-    .style('fill', function (d) {
-      if (d.children) return color(d.data.name)
-      if (d.parent) return color(d.parent.data.name)
-      return 0
+    .attr('fill', (d: any) => {
+      var dataCountCopy = d.copy().count()
+      let parentNoData = d?.parent?.data?.noData
+      let parentNoTarget = d?.parent?.data?.noTarget
+
+      if (d.data.noData || parentNoData) {
+        return colorNoValue(1)
+      } else if (d.data.noTarget || parentNoTarget) {
+        return colorNoValue(-1)
+      }
+      if (dataCountCopy.value > 1) {
+        return color(d.value / dataCountCopy.value) as any
+      }
+      return color(d.value) as any
     })
 
   function computeTextRotation(d: any) {
@@ -69,7 +149,7 @@ async function start() {
     return angle < 180 ? 'start' : 'end'
   }
 
-  graph
+  svg
     .selectAll('.node')
     .attr('text-anchor', function (d: any) {
       return computeTextPosition(d)
@@ -84,12 +164,10 @@ async function start() {
       return d.parent ? d.data.name : ''
     })
 
-  graph
+  svg
     .selectAll('.node')
     .append('title')
     .text((d: any) => {
-      console.log(d)
-
       return `${d
         .ancestors()
         .map((d: any) => d.data.name)
