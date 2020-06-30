a1ea3b89414bdbb8b0a799864cb90f8d4a48fd88
Christoffer A Traeen <christoffer.andersen@live.com>
2020-06-30 20:27:46 +0200

    Change scale type for no data nodes

--- src/js/helpers/Colors.ts
+++ src/js/helpers/Colors.ts
@@ -1,16 +1,16 @@
-import { scaleThreshold, scaleIdentity } from 'd3'
+import { scaleThreshold, scaleOrdinal } from 'd3'
 
 const colorsForValues = ['#d17d52', '#d8ae56', '#72b282', '#459a71']
 const colorsForNoValues = ['#89817b', '#609fba']
 
 const domainForValues = [32, 66, 95]
-const domainForNoValues = [0]
+const domainForNoValues = [1, 2]
 
 const colorScaleForValues = scaleThreshold<number, string>()
 	.domain(domainForValues)
 	.range(colorsForValues)
 
-const colorScaleForNoValues = scaleThreshold<number, string>()
+const colorScaleForNoValues = scaleOrdinal<number, string>()
 	.domain(domainForNoValues)
 	.range(colorsForNoValues)
 
