82bd3cf772f930c23ff2656a71e95c400277a30f
Christoffer A Traeen <christoffer.andersen@live.com>
2020-07-01 18:29:01 +0200

    Fix color issues

--- src/js/helpers/Colors.ts
+++ src/js/helpers/Colors.ts
@@ -1,9 +1,9 @@
 import { scaleThreshold, scaleOrdinal } from 'd3'
 
 const colorsForValues = ['#d17d52', '#d8ae56', '#72b282', '#459a71']
-const colorsForNoValues = ['#89817b', '#609fba']
+const colorsForNoValues = ['#609fba', '#89817b']
 
-const domainForValues = [32, 66, 95]
+const domainForValues = [33, 66, 95]
 const domainForNoValues = [1, 2]
 
 const colorScaleForValues = scaleThreshold<number, string>()
