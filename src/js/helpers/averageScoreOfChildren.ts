a8e0c43130ae0b987367758c3f36068861411944
Christoffer A Traeen <christoffer.andersen@live.com>
2020-08-17 09:03:20 +0200

    Fix random score calculation

--- src/js/helpers/averageScoreOfChildren.ts
+++ src/js/helpers/averageScoreOfChildren.ts
@@ -1,21 +1,15 @@
-import {
-	SmartCityPerformance,
-	TargetAvailable,
-} from '@lib/SmartCityPerformance'
+import { SmartCityPerformance, TargetAvailable } from '@lib/SmartCityPerformance'
 
 /**
  * Returns the average score of all the children scores upto the depth provided
  * @param node the node to get children scores of
  * @param depth how many sub children of children to go through, default = 1 = only direct children
  */
-export function getAverageChildScores(
-	node: SmartCityPerformance,
-	depth: number = 1
-): number {
+export function getAverageChildScores(node: SmartCityPerformance, depth: number = 1): number {
 	depth -= 1
 	let score = 0
 	if (node.targetAvailable == TargetAvailable.AVAILABLE) {
-		score += node.score ? node.score : 0
+		score += node.score && !node.children ? node.score : 0
 	}
 	if (depth < 0) return score
 	if (node.children) {
