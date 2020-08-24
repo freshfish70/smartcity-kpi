7822f9454d9d14f0415b0b51e509cd6c8cb0c128
freshfish70 <christoffer.andersen@live.com>
2020-08-24 09:29:14 +0200

    Fix ENV variables for build

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,9 @@ module.exports = {
 			{
 				test: /\.s[ac]ss$/i,
 				use: [
-					process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+					process.env.NODE_ENV !== 'production'
+						? 'style-loader'
+						: MiniCssExtractPlugin.loader,
 					// Translates CSS into CommonJS
 					'css-loader',
 					// Compiles Sass to CSS
